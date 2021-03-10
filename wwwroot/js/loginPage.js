47a062d818738c97f3c637f0404c4d402c2dc612
La Duc Hao <44287846+lamhaoloc@users.noreply.github.com>
2021-03-10 16:55:45 +0700

    update

---
+++ wwwroot/js/loginPage.js
@@ -0,0 +1,5 @@
+document.getElementById("auth-loading").style.display = "none";
+const handlePressAuthBtn = () => {
+  document.getElementById("auth-text").style.display = "none";
+  document.getElementById("auth-loading").style.display = "block";
+};
